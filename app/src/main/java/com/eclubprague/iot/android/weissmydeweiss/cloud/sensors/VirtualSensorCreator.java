5190faad30a10bbef9b61b1ec2b92c498c3b8d58
Pavel Valach <valach.pavel@gmail.com>
2015-07-15 15:03:07 +0200

    Changed Secret in Sensor to String

--- app/src/main/java/com/eclubprague/iot/android/weissmydeweiss/cloud/sensors/VirtualSensorCreator.java
+++ app/src/main/java/com/eclubprague/iot/android/weissmydeweiss/cloud/sensors/VirtualSensorCreator.java
@@ -2,7 +2,7 @@ package com.eclubprague.iot.android.weissmydeweiss.cloud.sensors;
 
 public class VirtualSensorCreator {
 
-	public static Sensor createSensorInstance(int uuid, SensorType type, int secret) {
+	public static Sensor createSensorInstance(int uuid, SensorType type, String secret) {
 		switch (type) {
 		case THERMOMETER : return new ESCThermometer(uuid, secret);
 		default : return null;
